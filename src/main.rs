7402b4d3511893690dacafa67ca6e3ad4d57bbe1
Lujia <guolujia0426@outlook.com>
2024-12-28 10:51:06 +0800

    Using two threads to reduce tearing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod animation;
 
 use std::io::stdout;
+use std::sync::mpsc::{self, Sender, Receiver};
+use std::thread;
 use std::time::Duration;
 
 use crossterm::cursor::{Hide, MoveTo, Show};
@@ -16,12 +18,23 @@ fn main() {
     setup_terminal();
     let color_primary = "\x1b[0m";
     let color_second = "\x1b[94m";
-    loop {
-        for frame in animation::ANIMATION_DATA.iter() {
-            render_frame(frame, color_primary, color_second);
-            key_press(30);
+
+    let (tx, rx): (Sender<()>, Receiver<()>) = mpsc::channel();
+
+    let key_press_tx = tx.clone();
+    thread::spawn(move || {
+        loop {
+            key_press(30, &key_press_tx);
         }
-    }
+    });
+
+    thread::spawn(move || {
+        loop {
+            for frame in animation::ANIMATION_DATA.iter() {
+                render_frame(&rx, frame, color_primary, color_second);
+            }
+        }
+    }).join().unwrap();
 }
 
 fn setup_terminal() {
@@ -31,25 +44,37 @@ fn setup_terminal() {
     enable_raw_mode().unwrap();
 }
 
-fn render_frame(frame: &[&str], color_primary: &str, color_second: &str) {
+fn render_frame(rx: &Receiver<()>, frame: &[&str], color_primary: &str, color_second: &str) {
     let mut y = 0;
     let (width, _) = size().unwrap();
     let x = if width > 100 { (width - 100) / 2 } else { 0 };
 
     let mut stdout = stdout();
-    stdout.execute(Clear(ClearType::All)).unwrap();
 
-    for &line in frame.iter() {
-        let colored_line = line
+    // for &line in frame.iter() {
+    //     let colored_line = line
+    //         .replace("<color>", color_second)
+    //         .replace("</color>", color_primary);
+    //     stdout.execute(MoveTo(x, y)).unwrap();
+    //     stdout.execute(Print(colored_line)).unwrap();
+    //     y += 1;
+    // }
+
+    let colored_frame = frame.iter().map(|&line|line
             .replace("<color>", color_second)
-            .replace("</color>", color_primary);
+            .replace("</color>", color_primary)).collect::<Vec<String>>();
+
+    rx.recv().unwrap();
+    stdout.execute(Clear(ClearType::All)).unwrap();
+    colored_frame.into_iter().for_each(|line|{
         stdout.execute(MoveTo(x, y)).unwrap();
-        stdout.execute(Print(colored_line)).unwrap();
+        stdout.execute(Print(line)).unwrap();
         y += 1;
-    }
+    });
+    
 }
 
-fn key_press(poll: u64) {
+fn key_press(poll: u64, tx: &Sender<()>) {
     if event::poll(Duration::from_millis(poll)).unwrap() {
         if let Event::Key(key_event) = event::read().unwrap() {
             if let KeyCode::Char('q') = key_event.code {
@@ -58,6 +83,7 @@ fn key_press(poll: u64) {
             }
         }
     }
+    tx.send(()).unwrap();
 }
 
 fn cleanup_terminal() {
