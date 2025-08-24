47768d00db57ed89cee1c9b4cfd164ddc16fa21d
sineptic <sineptic0@gmail.com>
2025-08-24 11:54:03 +0300

    refactor: remove unwraps

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{
     fs::File,
     io::{stdout, Read, Write},
     process::exit,
-    time::SystemTime,
+    time::Instant,
 };
 
 use animation::FRAMES;
@@ -10,51 +10,43 @@ use animation::FRAMES;
 mod animation;
 
 fn main() {
-    let _wrapper = alternate_screen_wrapper::unix::AlternateScreenOnStdout::enter()
+    let wrapper = alternate_screen_wrapper::unix::AlternateScreenOnStdout::enter()
         .unwrap()
         .unwrap();
 
-    std::thread::spawn(move || {
-        let mut stdin = std::io::stdin();
-        let mut buf = [0; 1024];
-        loop {
-            let count = stdin.read(&mut buf).unwrap();
-            if buf[..count].contains(&b'q') {
-                break;
-            }
-        }
-        let result = alternate_screen_wrapper::unix::restore_terminal();
-        if let Err(err) = result {
-            eprintln!("Error: {err}");
-            exit(1);
-        }
-        exit(0);
-    });
+    std::thread::spawn(handle_quit_event);
 
-    let start = SystemTime::now();
+    let res = run_animation();
+    drop(wrapper);
+    if let Err(err) = res {
+        eprintln!("Error: {err}");
+    }
+}
+
+fn run_animation() -> Result<(), std::io::Error> {
+    let start = Instant::now();
     const MICROS_PER_FRAME: u64 = 30_000;
 
     let mut stdout = stdout();
 
     // hide cursor
-    stdout.write_all(b"\x1B[?25l").unwrap();
+    stdout.write_all(b"\x1B[?25l")?;
     // disable line wrap
-    stdout.write_all(b"\x1B[?7l").unwrap();
+    stdout.write_all(b"\x1B[?7l")?;
     loop {
         // Automatically skip frames when render is slow
-        let frame_number =
-            (start.elapsed().unwrap().as_micros() / MICROS_PER_FRAME as u128) as usize;
+        let frame_number = (start.elapsed().as_micros() / MICROS_PER_FRAME as u128) as usize;
         let frame = &FRAMES[frame_number % FRAMES.len()];
         let rustix::termios::Winsize {
             ws_row: height,
             ws_col: width,
             ..
-        } = rustix::termios::tcgetwinsize(File::open("/dev/tty").unwrap()).unwrap();
+        } = rustix::termios::tcgetwinsize(File::open("/dev/tty")?)?;
         let width_gap = width.saturating_sub(animation::IMAGE_WIDTH) / 2;
         let height_gap = height.saturating_sub(animation::IMAGE_HEIGHT) / 2;
 
         // clear screen
-        stdout.write_all(b"\x1B[2J").unwrap();
+        stdout.write_all(b"\x1B[2J")?;
 
         for (i, line) in frame
             .iter()
@@ -62,17 +54,34 @@ fn main() {
             .take_while(|(i, _line)| *i as u16 <= height)
         {
             // move cursor
-            stdout
-                .write_fmt(format_args!(
-                    "\x1B[{};{}H",
-                    i as u16 + height_gap + 1, // height
-                    width_gap + 1,             // width
-                ))
-                .unwrap();
-            stdout.write_all(line.to_string().as_bytes()).unwrap();
+            stdout.write_fmt(format_args!(
+                "\x1B[{};{}H",
+                i as u16 + height_gap + 1, // height
+                width_gap + 1,             // width
+            ))?;
+            stdout.write_all(line.to_string().as_bytes())?;
         }
-        stdout.flush().unwrap();
+        stdout.flush()?;
 
         std::thread::sleep(std::time::Duration::from_micros(MICROS_PER_FRAME));
     }
+}
+
+fn handle_quit_event() {
+    let mut stdin = std::io::stdin();
+    let mut buf = [0; 1024];
+    loop {
+        let Ok(count) = stdin.read(&mut buf) else {
+            break;
+        };
+        if buf[..count].contains(&b'q') {
+            break;
+        }
+    }
+    let result = alternate_screen_wrapper::unix::restore_terminal();
+    if let Err(err) = result {
+        eprintln!("Error: {err}");
+        exit(1);
+    }
+    exit(0);
 }
